2f4e19f006271d9f32594709ecaa6339fc74a274
ayman Algamal <ayman01287985950@gmail.com>
2023-12-05 22:14:45 +0200

    initializing the environment and the Game class

---
+++ Game/src/com/ayman/fightEnemies/Game.java
@@ -0,0 +1,24 @@
+package com.ayman.fightEnemies;
+
+import java.awt.*;
+
+public class Game {
+
+    private static int width = 300;
+    private static int height = width / 12 * 8;
+    private static int scaleFactor = 3;
+
+
+
+
+    int getWidth() {
+        return width;
+    }
+    int getHeight() {
+        return height;
+    }
+    int getScaleFactor() {
+        return scaleFactor;
+    }
+
+}
