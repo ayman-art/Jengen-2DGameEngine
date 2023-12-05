eddf74cfb3dfa42481a135a5283078b8a03c7de9
ayman Algamal <ayman01287985950@gmail.com>
2023-12-05 22:16:57 +0200

    making the width and height final as they will not change after creation

--- Game/src/com/ayman/fightEnemies/Game.java
+++ Game/src/com/ayman/fightEnemies/Game.java
@@ -4,9 +4,9 @@ import java.awt.*;
 
 public class Game {
 
-    private static int width = 300;
-    private static int height = width / 12 * 8;
-    private static int scaleFactor = 3;
+    private static final int width = 300;
+    private static final int height = width / 12 * 8;
+    private static final int scaleFactor = 3;
 
 
 
