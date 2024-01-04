a5a8849c1cacf23256c3177112fd3cdfaaf8969a
Ayman Algamal <ayman01287985950@gmail.com>
2024-01-04 16:45:54 +0200

    Updating the move method in the Player class.

--- Game/src/com/ayman/fightEnemies/entity/mob/Player.java
+++ Game/src/com/ayman/fightEnemies/entity/mob/Player.java
@@ -16,10 +16,12 @@ public class Player extends Mob {
 
 
     public void update() {
-        if(input.up) y--;
-        if(input.down) y++;
-        if(input.left) x--;
-        if(input.right) x++;
+        int xa = 0, ya = 0;
+        if(input.up) ya--;
+        if(input.down) ya++;
+        if(input.left) xa--;
+        if(input.right) xa++;
+        if(xa != 0 || ya != 0) move(xa, ya);
 
 
     }
