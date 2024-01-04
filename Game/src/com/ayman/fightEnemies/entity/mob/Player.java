62c93ac7f9214efad5b9b1d82ea0e043d806a8cb
Ayman Algamal <ayman01287985950@gmail.com>
2024-01-04 14:03:40 +0200

    Creating the Player class

---
+++ Game/src/com/ayman/fightEnemies/entity/mob/Player.java
@@ -0,0 +1,21 @@
+package com.ayman.fightEnemies.entity.mob;
+
+public class Player extends Mob {
+
+    public Player() {
+
+    }
+    public Player(int x, int y) { //if we want to spawn the player at a specific location
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public void update() {
+
+    }
+
+    public void render() {
+
+    }
+}
