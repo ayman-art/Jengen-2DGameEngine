007bfdf9ca01db99a1f033dcafb3acafc273afa9
Ayman Algamal <ayman01287985950@gmail.com>
2024-01-04 14:03:08 +0200

    Creating the Mob class

---
+++ Game/src/com/ayman/fightEnemies/entity/mob/Mob.java
@@ -0,0 +1,34 @@
+package com.ayman.fightEnemies.entity.mob;
+
+import com.ayman.fightEnemies.Graphics.Sprite;
+import com.ayman.fightEnemies.entity.Entity;
+
+public abstract class Mob extends Entity {
+    protected Sprite sprite;
+    protected int dir = 0; //direction of the mob, popular convention :(0 = north, 1 = east, 2 = south, 3 = west)
+    protected boolean moving = false;
+
+    public void move(int xa, int ya) {
+        if(xa > 0) dir = 1; //east
+        if(xa < 0) dir = 3; //west
+        if(ya > 0) dir = 2; //south
+        if(ya < 0) dir = 0; //north
+
+        if(!collision()) {
+            x += xa;
+            y += ya;
+        }
+    }
+    public void update() {
+
+    }
+
+    public void render() {
+
+    }
+
+    private boolean collision() {
+        return false;
+    }
+
+}
