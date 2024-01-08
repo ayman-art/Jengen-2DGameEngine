09246c5a31b7b758453dbe354eec423b998fa6e1
Ayman Algamal <ayman01287985950@gmail.com>
2024-01-08 23:41:10 +0200

    Creating another Collision Detection Method which changes the Mob Frame.

--- Game/src/com/ayman/fightEnemies/entity/mob/Mob.java
+++ Game/src/com/ayman/fightEnemies/entity/mob/Mob.java
@@ -35,24 +35,35 @@ public abstract class Mob extends Entity {
     }
 
     private boolean collision(int xa, int ya) {
+        boolean solid = false;
+        int width = 16; //the width of the Mob collision box
+        int height = 16; //the height of the Mob collision box
+        int offSetX = -8; //the offset of the collision box from the Center of the Mob
+        int offSetY = -6; //the offset of the collision box from the Center of the Mob
+        for(int c = 0; c < 4; c++) {
+            int xt = ((x + xa) + c % 2 * width + offSetX) >> 4; //the x coordinate of the tile the Mob is colliding with
+            int yt = ((y + ya) + c / 2 * height + offSetY) >> 4; //the y coordinate of the tile the Mob is colliding with
+            if(level.getTile(xt, yt).isSolid()) solid = true;
+        }
+        return solid;
+    }
+
+
+    private boolean collisionEntire(int xa, int ya) {
         int size = 16; //the size of the sprite
-        int[][] corners = new int[][] { //the corners of the sprite
-                {-size, -size}, {size-1, -size}, {-size, size-1}, {size-1, size-1},
-                {0, size-1}, {0, -size}, {-size, 0}, {size-1, 0}
+        int[][] corners = new int[][]{ //the corners of the sprite
+                {-size, -size}, {size - 1, -size}, {-size, size - 1}, {size - 1, size - 1},
+                {0, size - 1}, {0, -size}, {-size, 0}, {size - 1, 0}
         };
-        for(int[] corner : corners) {
+        for (int[] corner : corners) {
             int xc = x + corner[0] + xa;
             int yc = y + corner[1] + ya;
-            System.out.println((xc>>4) + ", " + (yc>>4));
-            System.out.println(level.getTile(xc>>4, yc>>4));
-            if(level.getTile(xc>>4, yc>>4).isSolid()) {
+            System.out.println((xc >> 4) + ", " + (yc >> 4));
+            System.out.println(level.getTile(xc >> 4, yc >> 4));
+            if (level.getTile(xc >> 4, yc >> 4).isSolid()) {
                 return true;
             }
         }
-
-
-
         return false;
     }
-
 }
