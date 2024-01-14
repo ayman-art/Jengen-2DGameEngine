61c30331ddf73c6814a13575cb698cd0abe5e929
Ayman Algamal <ayman01287985950@gmail.com>
2024-01-14 11:32:54 +0200

    Adding more fields and methods for Particle Class.

--- Game/src/com/ayman/fightEnemies/entity/particle/Particle.java
+++ Game/src/com/ayman/fightEnemies/entity/particle/Particle.java
@@ -1,5 +1,6 @@
 package com.ayman.fightEnemies.entity.particle;
 
+import com.ayman.fightEnemies.Graphics.Screen;
 import com.ayman.fightEnemies.Graphics.Sprite;
 import com.ayman.fightEnemies.entity.Entity;
 
@@ -11,12 +12,14 @@ public class Particle extends Entity {
     private List<Particle> particles = new ArrayList<Particle>();
     Sprite sprite;
     private int life;
+    private double xDouble, yDouble;
     private double xVel, yVel;
 
 
     public Particle(int x, int y, int life) {
-        this.x = x;
-        this.y = y;
+
+        this.xDouble = this.x = x;
+        this.yDouble = this.y = y;
         this.life = life + (random.nextInt(20) - 10);
         sprite = Sprite.basicParticle;
         this.xVel = random.nextGaussian();
@@ -30,6 +33,19 @@ public class Particle extends Entity {
         for(int i = 0; i < amount - 1; i++) {
             particles.add(new Particle(x, y, life));
         }
+    }
+
+    public void update() {
+        xDouble += xVel;
+        yDouble += yVel;
+
+    }
+
+    public void render(Screen screen) {
+        for(int i = 0; i < particles.size(); i++) {
+            particles.get(i).render(screen);
+        }
+        screen.renderSprite(x, y, sprite, true);
     }
 
 }
