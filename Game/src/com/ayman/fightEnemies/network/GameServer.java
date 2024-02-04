cd2a4b04b035e0e11d96502348c529b2a7a8ab17
Ayman Algamal <ayman01287985950@gmail.com>
2024-02-04 20:48:28 +0200

    Beginning the server.

--- Game/src/com/ayman/fightEnemies/network/GameServer.java
+++ Game/src/com/ayman/fightEnemies/network/GameServer.java
@@ -4,6 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.net.DatagramSocket;
 import java.util.List;
+import java.util.Scanner;
 
 public class GameServer extends Thread {
 
@@ -12,8 +13,32 @@ public class GameServer extends Thread {
         private List<GameClient> clients;
         private Thread run, send, receive;
 
-        public GameServer() {
+        public GameServer(int port) {
+                this.port = port;
+                try {
+                        socket = new DatagramSocket(port);
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
 
+                run = new Thread(this, "Server");
+
+
+        }
+
+        public void run() {
+                System.out.println("Server started on port: " + port);
+                while(true) {
+                        System.out.println("Server is running...");
+                }
+        }
+
+
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter port number: ");
+                int port = scanner.nextInt();
+                new GameServer(port).start();
         }
 
 }
