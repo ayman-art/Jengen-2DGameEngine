9a130c732f09a9be58579322ef36c8a557bdbf2a
Ayman Algamal <ayman01287985950@gmail.com>
2024-04-15 19:07:46 +0200

    Adding Context for SpawnLevel.

--- Game/src/com/ayman/fightEnemies/game/contexts/levelcontexts/SpawnLevelContext.java
+++ Game/src/com/ayman/fightEnemies/game/contexts/levelcontexts/SpawnLevelContext.java
@@ -1,4 +1,41 @@
 package com.ayman.fightEnemies.game.contexts.levelcontexts;
 
+import com.ayman.fightEnemies.game.contexts.AIContext;
+
 public class SpawnLevelContext implements ILevelContext{
+
+    private final int numberOfLevels;
+    private final String levelsLocation;
+
+    private SpawnLevelContext(Builder builder){
+        this.numberOfLevels = builder.numberOfLevels;
+        this.levelsLocation = builder.levelsLocation;
+    }
+
+    public int getNumberOfLevels(){
+        return numberOfLevels;
+    }
+
+    public String getLevelsLocation(){
+        return levelsLocation;
+    }
+    public static class Builder{
+        private int numberOfLevels;
+        String levelsLocation;
+
+        public Builder numberOfLevels(int numberOfLevels){
+            this.numberOfLevels = numberOfLevels;
+            return this;
+        }
+
+        public Builder levelsLocation(String levelsLocation){
+            this.levelsLocation = levelsLocation;
+            return this;
+        }
+
+        public SpawnLevelContext build(){
+            return new SpawnLevelContext(this);
+        }
+    }
+
 }
